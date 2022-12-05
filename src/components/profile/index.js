32a81c079bf217c9ca3e84b89d62f425cabb66ec
ujjvalksharma <ujjvalksharma@gmail.com>
2022-12-04 22:05:04 -0500

    Implement user likes

--- src/components/profile/index.js
+++ src/components/profile/index.js
@@ -1,7 +1,36 @@
 import React from "react";
+import {Routes, Route} from "react-router-dom";
+/*
+import MyTuits from "./my-tuits";
+import TuitsAndReplies
+  from "./tuits-and-replies";
+import Media from "./media"; */
+import MyLikes from "./my-likes";
+
 const Profile = () => {
+
+  const logout =()=>{
+    alert('logout button clicked');
+  }
   return(
-    <h1>Profile</h1>
+    <div>
+      <h4>{"ujjval"}</h4>
+      <h6>@{"ujjval"}</h6>
+      <button onClick={()=> logout()}>
+        Logout</button>
+
+      <Routes>
+    {/*      <Route path="/mytuits"
+               element={<MyTuits/>}/>
+      <Route path="/tuits-and-replies"
+               element={<TuitsAndReplies/>}/> 
+        <Route path="/media"
+               element={<Media/>}/> */}
+        <Route path="profile/mylikes"
+               element={<MyLikes/>}/>
+      </Routes>
+
+    </div>
   );
-}
+};
 export default Profile;
