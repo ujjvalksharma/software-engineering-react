791e512301ae036decd35330fdf1ea3e6d5c0dc8
ujjvalksharma <ujjvalksharma@gmail.com>
2022-12-07 05:00:03 -0500

    refactor tuits service test

--- src/tests/tuits-service.test.js
+++ src/tests/tuits-service.test.js
@@ -1,5 +1,5 @@
 
- import TuitService from "../services/tuits-service";
+/*import TuitService from "../services/tuits-service";
  import UserService from "../services/user-service";
   describe('can create tuit wtih REST API', () => {
   
@@ -24,7 +24,7 @@
     });
   
   
-    });
+    }); */
     /*
     describe('can delete tuit wtih REST API', () => {
       
@@ -75,7 +75,7 @@
       });
     });
     */
-    describe('can retrieve all tuits with REST API', () => {
+    /*describe('can retrieve all tuits with REST API', () => {
       
       const tuits = [
   
@@ -129,4 +129,88 @@
       });
   
   
+    });*/
+
+
+    import TuitService, { createTuit } from "../services/tuits-service";
+
+    describe('can create tuit with REST API', () => {
+      // TODO: implement this
+
+      let oldTuit = {
+        tuit: "Hi! This is my new tuit"
+      };
+      let userId='638f5daf5561b563b87b7577';
+
+      test('can create tuit with REST API', async () => {
+
+      //  const createTuit = async () =>{
+
+          const createdTuit= await TuitService.createTuit(userId,oldTuit);
+        //  console.log('createdTuit: '+JSON.stringify(createdTuit));
+          expect(createdTuit.tuit).toEqual(oldTuit.tuit);
+          expect(createdTuit.postedBy).toEqual(userId);
+          const deletedTuit= await TuitService.deleteTuit(createdTuit._id);
+          expect(deletedTuit.data.deletedCount).toEqual(1);
+     //   };
+
+       // createTuit();
+      });
+
+
+    });
+    
+    describe('can delete tuit wtih REST API', () => {
+      let oldTuit = {
+        tuit: "Hi! This is my new tuit"
+      };
+      let userId='638f5daf5561b563b87b7577';
+
+      test('can create tuit with REST API', async () => {
+
+
+
+          const createdTuit= await TuitService.createTuit(userId,oldTuit);
+        //  console.log('createdTuit: '+JSON.stringify(createdTuit));
+          expect(createdTuit.tuit).toEqual(oldTuit.tuit);
+          expect(createdTuit.postedBy).toEqual(userId);
+         const deletedTuit= await TuitService.deleteTuit(createdTuit._id);
+         expect(deletedTuit.data.deletedCount).toEqual(1);
+
+      });
+
+    });
+    
+    describe('can retrieve a tuit by their primary key with REST API', () => {
+      
+      let oldTuit = {
+        tuit: "Hi! This is my new tuit"
+      };
+      let userId='638f5daf5561b563b87b7577';
+
+      test('can create tuit with REST API', async () => {
+
+          const createdTuit= await TuitService.createTuit(userId,oldTuit);
+          expect(createdTuit.tuit).toEqual(oldTuit.tuit);
+          expect(createdTuit.postedBy).toEqual(userId);
+         const lookedUpTuitById= await TuitService.findTuitById(createdTuit._id)
+         expect(createdTuit.tuit).toEqual(lookedUpTuitById.tuit);
+          expect(lookedUpTuitById.postedBy).toEqual(userId);
+          const deletedTuit= await TuitService.deleteTuit(createdTuit._id);
+         expect(deletedTuit.data.deletedCount).toEqual(1);
+
+      });
+
+
+    });
+    
+    describe('can retrieve all tuits with REST API', () => {
+      
+      test('can create tuit with REST API', async () => {
+
+        const allTuits= await TuitService.findAllTuits();
+        expect(allTuits.length>0).toEqual(true);
+
+    });
+
     });
